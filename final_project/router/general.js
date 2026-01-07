7b8b1cabeee7a3f067aafe54f40f13dc15771b13
Prince YOMBI <yombifanny@gmail.com>
2026-01-07 01:22:14 +0100

    tcahe 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,17 +42,30 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+
+
+// Route pour obtenir les détails d’un livre par ISBN (Promesse)
+public_users.get("/isbn/:isbn", (req, res) => {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
-    return res.status(200).json(book);
-  }else { 
-    return res.status(404).json({message: "Book not found"});
-  } 
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+
+  // Simuler un appel asynchrone avec Axios (ici on appelle notre propre API)
+  axios.get("http://localhost:5000/")
+    .then(response => {
+      const allBooks = response.data;
+      const book = allBooks[isbn];
+      if (book) {
+        res.json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Erreur lors de la récupération", error: error.message });
+    });
+});
+
+module.exports = public_users;
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
